189018ff35161be7255931abf825b88c37b296eb
015Tommy <302181697@student.rocmondriaan.nl>
2024-09-15 19:26:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const firstNumber = 5;
+const secondNumber = "5";
+
+if (firstNumber == secondNumber) { //Dit is waar, omdat er NIET naar het datatype gekeken wordt
+    console.log('The numbers are equal');
+}
