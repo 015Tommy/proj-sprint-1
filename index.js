0696d0bbe44c6309ab1d547a575e2b3191443607
015Tommy <302181697@student.rocmondriaan.nl>
2024-09-17 17:55:24 +0200

    sprint 1

--- index.js
+++
@@ -1,6 +0,0 @@
-const firstNumber = 5;
-const secondNumber = "5";
-
-if (firstNumber == secondNumber) { //Dit is waar, omdat er NIET naar het datatype gekeken wordt
-    console.log('The numbers are equal');
-}
---
+++ main.js
@@ -0,0 +1,6 @@
+const firstNumber = 5;
+const secondNumber = "5";
+
+if (firstNumber == secondNumber) { 
+    console.log('The numbers are equal');
+}
