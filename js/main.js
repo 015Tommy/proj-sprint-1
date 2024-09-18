3a712e56f2e6ac5d3a34904846b980bb01805c5a
015Tommy <302181697@student.rocmondriaan.nl>
2024-09-18 12:52:51 +0200

    extra

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('Main loaded');
+let playerScore;
+
+
+
+let diceOne = getRandomInt(6);
+console.log(diceOne);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+ 
+let diceTwo = getRandomInt(6);
+console.log(diceTwo);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
--- main.js
+++
@@ -1,8 +0,0 @@
-const firstNumber = 5;
-const secondNumber = "5";
-const number = true;
-
-if (firstNumber == secondNumber) { 
-    console.log('The numbers are equal');
-}
-
